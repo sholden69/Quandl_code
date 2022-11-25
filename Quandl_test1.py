0a83410e891981a1eb0a78a7342322656261caab
Simon Holden <simon.holden@gmail.com>
2022-11-25 16:12:08 +0000

    added plot

--- Quandl_test1.py
+++ Quandl_test1.py
@@ -21,7 +21,7 @@ print("------------------------------------------------------------------")
 # Source MS, GS, BAC stock prices for the last 20 years
 data2 = quandl.get_table('WIKI/PRICES', ticker = bank_tickers,
                         qopts = { 'columns': ['ticker', 'date', 'adj_close'] },
-                        date = { 'gte': '2000-01-01', 'lte': '2020-01-10' },
+                        date = { 'gte': '2000-01-01', 'lte': '2022-01-11' },
                         paginate=True)
 #print(data2)
 
@@ -32,6 +32,10 @@ new = data2.set_index('date')
 clean_data = new.pivot(columns='ticker')
 clean_data.info()
 
+
 # check the head of the output
 print(clean_data)
-
+plt.plot(clean_data)
+plt.legend(bank_tickers,loc='upper left')
+plt.title('US Bank Stocks 2000-2022')
+plt.show()
